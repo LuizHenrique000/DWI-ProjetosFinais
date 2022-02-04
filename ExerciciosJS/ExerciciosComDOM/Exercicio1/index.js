24cc813f5d100927bf512f78a3e637dacb63f0e3
LuizHenrique000 <luizlhra24@gmail.com>
2022-02-04 20:26:10 -0300

    Finaliza API e adiciona a href ao html principal

--- ExerciciosJS/ExerciciosComDOM/Exercicio1/index.js
+++ ExerciciosJS/ExerciciosComDOM/Exercicio1/index.js
@@ -1,7 +1,14 @@
 function somar() {
-    valor1 = document.getElementById('valor1').value;
-    valor2 = document.getElementById('valor2').value;
-    valor3 = document.getElementById('valor3').value;
-    resultado = parseInt(valor1) + parseInt(valor2) + parseInt(valor3);
+    console.log("iniciando")
+    let valor1 = validarNaoNumerico(document.getElementById('valor1').value);
+    let valor2 = validarNaoNumerico(document.getElementById('valor2').value);
+    let valor3 = validarNaoNumerico(document.getElementById('valor3').value);
+    resultado = Number(valor1) + Number(valor2) + Number(valor3);
     document.getElementById("Resultado1").value = resultado;
+}
+function validarNaoNumerico(valor) {
+    if (isNaN(valor) || !valor)
+        return 0;
+    console.log(valor)
+    return valor;
 }
